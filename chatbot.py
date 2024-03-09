206bb72115546f529421400b3f65b2ab33c4bf5a
Arnab Barua <41656485+khavjhav@users.noreply.github.com>
2024-03-10 02:01:41 +0600

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -4,14 +4,14 @@ import requests
 from assistant import chat_generator
 st.title("💬 Smartgrad")
 
-hide_st_style = """
-            <style>
-            MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+#hide_st_style = """
+ #           <style>
+  #          MainMenu {visibility: hidden;}
+   #         footer {visibility: hidden;}
+    #        header {visibility: hidden;}
+     #       </style>
+      #      """
+#st.markdown(hide_st_style, unsafe_allow_html=True)
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
